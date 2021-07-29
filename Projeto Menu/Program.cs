42d7bc4e23121a09c7d5994e4b503c931beed028
VALNEI REIS <valnei.reis01@etec.sp.gov.br>
2021-07-29 13:11:13 -0300

    Novas mudanças no aplicativo, agora com uso do While.

--- Projeto Menu/Program.cs
+++ Projeto Menu/Program.cs
@@ -10,35 +10,61 @@ namespace Projeto_Menu
     {
         static void Main(string[] args)
         {
-            string opcao;
+            int opcao = 0;
 
-            // Carrega o método MontaTela
-            MontaTela();
-
-            Console.SetCursorPosition(03, 05);
-            Console.WriteLine("Menu de opções");
-            Console.SetCursorPosition(03, 06);
-            Console.WriteLine("Escolha entre as opções abaixo um número para executar");
-            Console.SetCursorPosition(03, 07);
-            Console.WriteLine("o procedimento escolhido.");
-
-            Console.SetCursorPosition(25, 10);
-            Console.WriteLine("1 - Calculadora");
-            Console.SetCursorPosition(25, 11);
-            Console.WriteLine("2 - Sair");
-
-            Console.SetCursorPosition(25, 14);
-            Console.WriteLine("Digite a opção desejada :");
-            // Ainda falta posicionar o cursor para a variavel opcao abaixo.
-            opcao = Console.ReadLine();
-
-            if (opcao == "1")
+            while (opcao != 4)
             {
-                Calculadora();
+                // Carrega o método MontaTela
+                MontaTela();
+
+                Console.SetCursorPosition(03, 05);
+                Console.WriteLine("Menu de opções");
+                Console.SetCursorPosition(03, 06);
+                Console.WriteLine("Escolha entre as opções abaixo um número para executar");
+                Console.SetCursorPosition(03, 07);
+                Console.WriteLine("o procedimento escolhido.");
+
+                Console.SetCursorPosition(25, 10);
+                Console.WriteLine("1 - Calculadora");
+                Console.SetCursorPosition(25, 11);
+                Console.WriteLine("2 - Iniciar Vendas");
+                Console.SetCursorPosition(25, 12);
+                Console.WriteLine("3 - Consultar Vendas");
+                Console.SetCursorPosition(25, 13);
+                Console.WriteLine("4 - Sair");
+
+                Console.SetCursorPosition(25, 16);
+                Console.WriteLine("Digite a opção desejada :");
+                Console.SetCursorPosition(52, 16);
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        {
+                            Calculadora();
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            IniciarVendas();
+                            break;
+                        }
+                    case 3:
+                        {
+                            ConsultarVendas();
+                            break;
+                        }
+                    case 4:
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                }
+
+                Console.ReadKey();
             }
-
-
-            Console.ReadKey();
         }
         
 
@@ -48,9 +74,32 @@ namespace Projeto_Menu
             Console.SetCursorPosition(03,05);
             Console.WriteLine("Você escolheu o módulo calculadora");
 
+            Console.SetCursorPosition(03, 08);
+            Console.WriteLine("Pressione ENTER para voltar ao menu de opções.");
             Console.ReadKey();
         }
 
+        static void IniciarVendas()
+        {
+            MontaTela();
+            Console.SetCursorPosition(03, 05);
+            Console.WriteLine("Você escolheu o módulo Iniciar Vendas");
+
+            Console.SetCursorPosition(03, 08);
+            Console.WriteLine("Pressione ENTER para voltar ao menu de opções.");
+            Console.ReadKey();
+        }
+
+        static void ConsultarVendas()
+        {
+            MontaTela();
+            Console.SetCursorPosition(03, 05);
+            Console.WriteLine("Você escolheu o módulo Consultar Vendas");
+
+            Console.SetCursorPosition(03, 08);
+            Console.WriteLine("Pressione ENTER para voltar ao menu de opções.");
+            Console.ReadKey();
+        }
         static void MontaTela()
         {
             // Inicia as variáveis para borda horizontal, vertical e controles de coluna e linha
